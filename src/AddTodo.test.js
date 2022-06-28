508dc6548fa0b60e405b4a65ddc3dc2ac082e277
Kiran Aziz <kiransaziz@gmail.com>
2022-06-28 13:34:21 -0400

    Committing files

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -20,24 +20,107 @@ afterEach(() => {
 
 
  test('test that App component doesn\'t render dupicate Task', () => {
-  render(<App />);
+  render(<App />); //Mocks the component so that we can do the testing.
+
+  //Looks for a textbox component with the words "Add New Item"
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  //Types the value "History Test" into the text box.
+  fireEvent.change(inputTask, { target: { value: "History Test"}}); 
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+
+  //Clicks the selected element.
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, { target: { value: "History Test"}}); 
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  //Searches for "History Test" on the screen ignoring case using regex. 
+  //(Note: getBy only looks for one value. If more than one value or 
+  //no value is present then an error will occur. If you want to get more 
+  //then use getAllBy).
+  //Returns an array of all elements matching the query.
+  const check = screen.getAllByText(/History Test/i);
+
+  //The element should be in the page if it is the test case is passed. 
+  //Otherwise the test fails.
+  expect(check.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputTask, { target: { value: null}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: null}});
+  fireEvent.click(element);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
 
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const checkBox = screen.getByRole('checkbox'); 
+  fireEvent.click(checkBox);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "06/28/2022";
+
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const dueDate2 = "06/29/2022";
+  fireEvent.change(inputTask, { target: { value: "Math Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate2}});
+  fireEvent.click(element);
+
+
+  const historyCheck = screen.getByTestId(/History Test/i).style.background;
+  const mathCheck = screen.getByTestId(/Math Test/i).style.background;
+  expect(historyCheck).toBe("rgb(255, 0, 0)");
+  expect(mathCheck).not.toBe("rgb(255, 0, 0)");
  });
